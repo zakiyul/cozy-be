fed754d3c13c7a76f7852b49b105e5d24cd20354
zakiyul <zakiyul.py@gmail.com>
2023-01-22 16:35:19 +0800

    add detail space

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('recommended-spaces/', views.space_recomendation)  
+    path('recommended-spaces/', views.space_recomendation),
+    path('recommended-spaces/<int:id>', views.detail_space_recomendation)
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -11,3 +11,27 @@ def space_recomendation(req):
     spaces = models.Space.objects.all()
     serializer = serializers.SpaceSerializer(spaces, many=True)
     return Response(serializer.data)
+
+@api_view(['GET', 'PATCH', 'DELETE'])
+def detail_space_recomendation(req, id):
+    try:
+        space = models.Space.objects.get(pk=id)
+    except models.Space.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    if req.method == 'GET':
+        serializer = serializers.SpaceSerializer(space)
+        return Response(serializer.data)
+
+    elif req.method == 'PATCH':
+        serializer = serializers.SpacePhotoSerializer(space, data=req.data, partial=True)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif  req.method == 'DELETE':
+        space.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
